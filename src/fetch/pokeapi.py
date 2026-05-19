1a3911725aea0eb85595b7b83bc96814ed714eb8
Mario Baravetto <baravettomario@gmail.com>
2026-05-19 22:07:43 +0200

    created my own pokeapi pokemon request, tested responsed on notebook

--- src/fetch/pokeapi.py
+++ src/fetch/pokeapi.py
@@ -4,30 +4,25 @@ BASE_URL = "https://pokeapi.co/api/v2"
 
 def get_pokemon(species: str | int) -> dict:
     """
-    Fetch Pokémon data from the PokeAPI for a given species name.
-    
-    Args:
-        species (str | int): The name or ID of the Pokémon species (e.g., "pikachu" or 25).
-    
-    Returns:
-        dict: A dictionary containing the Pokémon data, or None if not found.
+    Fetch pokemon data from pokeapi url
+
+    Args: species (str | int): the name of the pokemon
+
+    Returns: the json dictionary with the pokemon info
     """
+
     if isinstance(species, int):
-        species = str(species).lower()
-    else:
-        species = species.lower()
-    
-    url = f"{BASE_URL}/pokemon/{species}"
+        species = str(species)
+
+    species = species.lower()
+
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an error for bad status codes
-        return response.json()
-    
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
+        pokemon = requests.get(f'{BASE_URL}/pokemon/{species}')
+        return pokemon.json()
+
     except Exception as err:
-        print(f"An error occurred: {err}")
-    return None
+        print(f'An error occurred: {err}')
+
 
 
 if __name__ == "__main__":
